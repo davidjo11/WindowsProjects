16b0c6058990111a95d87d9c370c0b1cde1e552f
DavidTheProcrastinator <dav.josias@gmail.com>
2016-01-25 13:46:21 +0100

    First commit: ignorant => gitignore comming soon

---
+++ Project_DotNET/Project_DotNET/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Project_DotNET.MvcApplication" Language="C#" %>
---
+++ Project_DotNET/Project_DotNET/Models/User.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_DotNET.Models
+{
+    class User
+    {
+        public int id { get; }
+
+        public String firstName { get; set; }
+
+        public String lastName { get; set; }
+
+        public DateTime birthday { get; set; }
+
+        public DateTime firstDay { get; set; }
+    }
+}
